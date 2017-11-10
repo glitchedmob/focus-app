8e098e0df1d864beee8c9263d77445f6aa107bb6
Levi Zitting <levizitting5663@gmail.com>
2017-11-10 13:08:13 -0600

    Creating basic landing page without styles

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,5 +2,6 @@ const mix = require('laravel-mix');
 mix.pug   = require('laravel-mix-pug');
 
 mix
+	.sourceMaps()
 	.sass('src/docs/sass/main.scss', 'docs/css')
 	.pug('src/docs/pug/*.pug', 'docs', { seeds: null })
