d6d6d3c53653dea9841b14257b2513c6640235c2
Levi Zitting <levizitting5663@gmail.com>
2017-11-11 10:47:20 -0600

    Adding build folder to repo to prevent laravel-mix-pug errors and adding pug to app

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,7 +3,10 @@ mix.pug   = require('laravel-mix-pug');
 
 mix
 	.sourceMaps()
+	// GitHub Pages files
 	.sass('src/docs/sass/main.scss', 'docs/css')
 	.pug('src/docs/pug/*.pug', 'docs', { seeds: null })
+	// Extension files
 	.copyDirectory('src/app/images', 'build/images')
 	.copy(['src/app/manifest.json'], 'build')
+	.pug('src/app/pug/*.pug', 'build/html', { seeds: null })
