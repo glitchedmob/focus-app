5bce59393456a76d2afeb3efb7165a4f38d6a106
Levi Zitting <levizitting5663@gmail.com>
2017-11-16 20:32:15 -0600

    Icon now changes based on focus state

--- src/app/ts/popup/FocusToggle.ts
+++ src/app/ts/popup/FocusToggle.ts
@@ -1,6 +1,10 @@
 import { qs, $on } from "../helpers";
 
+declare const chrome: any;
 
+/**
+ * Manages the focus toggle in the popup
+ */
 export class FocusToggle
 {
 	toggleCheckbox: Element;
@@ -9,9 +13,11 @@ export class FocusToggle
 	
 	constructor()
 	{
+		// Chache main HTML elements in popup
 		this.titleEl = qs('#title')
 		this.toggleCheckbox = qs('#focus-state');
 
+		// Attache click event listener
 		$on(this.toggleCheckbox, 'click', this.toggleFocus.bind(this));
 	}
 
@@ -19,8 +25,15 @@ export class FocusToggle
 	// the state of the focused property
 	toggleFocus(): void
 	{
+		// Updatee internal focused state
 		this.focused = !this.focused;
-
+		
+		// Update css class
 		this.titleEl.className = this.focused ? 'focused' : '';
+
+		// Update chrome icon
+		chrome.browserAction.setIcon({
+			path: this.focused ? '../images/icons/focus-app38.png' : '../images/icons/focus-app-red38.png'
+		});
 	}
 }
