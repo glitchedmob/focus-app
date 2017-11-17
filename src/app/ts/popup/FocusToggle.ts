4037505f61b6f4734ada2e2f7c68639663f4c19d
Levi Zitting <levizitting5663@gmail.com>
2017-11-16 18:45:58 -0600

    Getting title to change focus when toggle is clicked

--- src/app/ts/popup/FocusToggle.ts
+++ src/app/ts/popup/FocusToggle.ts
@@ -1,7 +1,26 @@
+import { qs, $on } from "../helpers";
+
+
 export class FocusToggle
 {
+	toggleCheckbox: Element;
+	titleEl: Element;
+	focused = false;
+	
 	constructor()
 	{
-		console.log('Hello World');
+		this.titleEl = qs('#title')
+		this.toggleCheckbox = qs('#focus-state');
+
+		$on(this.toggleCheckbox, 'click', this.toggleFocus.bind(this));
+	}
+
+	// Toggles the focused class based on 
+	// the state of the focused property
+	toggleFocus(): void
+	{
+		this.focused = !this.focused;
+
+		this.titleEl.className = this.focused ? 'focused' : '';
 	}
 }
