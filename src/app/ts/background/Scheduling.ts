03bc6225637089ebfe1b2bdde8949644f931518b
Levi Zitting <levizitting5663@gmail.com>
2017-11-29 17:39:51 -0600

    WIP on scheduleing backend. Delaying for now until I can rethink schedule storage structure and use moments.js

--- src/app/ts/background/Scheduling.ts
+++ src/app/ts/background/Scheduling.ts
@@ -3,9 +3,12 @@ import { Storage } from './../extension/Storage';
 export class Scheduling {
 
 	private schedule: null | any = null;
+	private weekDays = [
+		'sunday', 'monday', 'tuesday',
+		'wednesday', 'thursday', 'friday', 'saturday'
+	];
 
 	constructor() {
-		this.createAlarms();
 		this.addEvents();
 		this.updateDataFromStorage();
 	}
@@ -17,11 +20,56 @@ export class Scheduling {
 
 	private addEvents() {
 		chrome.storage.onChanged.addListener(details => {
-			console.log(details);
+			if (!details.hasOwnProperty('schedule')) return;
+
+			Storage.get('schedule', 'sync').then(schedule => {
+				this.schedule = schedule;
+				this.buildSchedule();
+			});
 		});
 	}
 
-	private createAlarms() {
+	private buildSchedule() {
+		this.clearAlarms();
+
+		const today = new Date();
+
+		let weekDay = this.getWeekDay(today);
+
+		for (let i = 0; i < 6; i++) {
+			if (this.schedule[weekDay].length === 0) {
+				weekDay = this.nextWeekday(weekDay);
+			} else {
+				for (const time of this.schedule[weekDay]) {
+					console.log(time);
+				}
+			}
+
+		}
+
+	}
+
+	private nextWeekday(weekDay: string): string {
+		const dayIndex = this.weekDays.indexOf(weekDay);
+
+		if (dayIndex === 6) return this.weekDays[0];
+
+		return this.weekDays[dayIndex + 1];
+	}
+
+	private getWeekDay(today: Date): string {
+		return this.weekDays[today.getDay()];
+	}
+
+	private getTimeAMPM(today: Date): string {
+		return today.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+	}
+
+	private clearAlarms() {
+		console.log('cleared');
+	}
+
+	private createAlarms(start: number, end: number) {
 		console.log('alarms');
 	}
 
